eaa603c79ec5ed5e3d1e924fa73ca055af9684af
kongfan <kongfan@kingsoft.com>
2022-08-27 15:27:50 +0800

    feat: 003 connect keyboard input with player walk movement

--- Assets/Scripts/ActorController.cs
+++ Assets/Scripts/ActorController.cs
@@ -1,25 +1,34 @@
+using System;
 using UnityEngine;
 
 public class ActorController : MonoBehaviour
 {
     public GameObject model;
     public PlayerInput pi;
+    public float walkSpeed = 2.0f;
 
     [SerializeField] 
     private Animator anim;
+    private Rigidbody rigid;
+    private Vector3 movingVec;
 
     private void Awake()
     {
         pi = GetComponent<PlayerInput>();
         anim = model.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
         anim.SetFloat("forward", pi.Dmag);
-        if (pi.Dmag > 0.1f)
-        {
-            model.transform.forward = pi.Dvec;
-        }
+        if (pi.Dmag > 0.1f) model.transform.forward = pi.Dvec;
+        movingVec = model.transform.forward * (pi.Dmag * walkSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        // rigid.position += movingVec * Time.fixedDeltaTime;  // 修改 rigid.position 移动角色
+        rigid.velocity = new Vector3(movingVec.x, rigid.velocity.y, movingVec.z);   // 修改 rigid.velocity 移动角色
     }
 }
