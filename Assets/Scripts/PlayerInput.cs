1171d885c4452aec9959f495765cfc22ddba6364
kongfan <kongfan@kingsoft.com>
2022-08-27 15:04:29 +0800

    feat: 001 receive keyboard input

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public string keyUp = "w";
+    public string keyDown = "s";
+    public string keyLeft = "a";
+    public string keyRight = "d";
+
+    public float Dup;
+    public float Dright;
+
+    public bool inputEnable = true;
+    
+    private float targetDup;
+    private float targetDright;
+    private float velocityDup;
+    private float velocityDright;
+
+    private void Update()
+    {
+        targetDup = (Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);
+        targetDright = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
+
+        if (inputEnable == false)
+        {
+            targetDup = 0;
+            targetDright = 0;
+        }
+
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
+    }
+}
